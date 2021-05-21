cb44ae8c1c19bf3a910e65f1f72ae5f6e5f5769c
kadwait <82862860+kadwait@users.noreply.github.com>
2021-05-21 13:06:43 +0545

    new files added

---
+++ index.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+		
+	var menuList = document.getElementById("menuList");
+				
+				menuList.style.maxheight="0px"
+				
+				function togglemenu()
+				{
+					if(menuList.style.maxheight=="0px")
+				{
+					menuList.style.maxheight="130px";
+				}
+					else
+				{
+					menuList.style.maxheight="0px";
+
+				}
+	</script>	s
